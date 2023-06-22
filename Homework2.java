154aff5f709ebeacfe30e5073a39d15e9820a085
DariaLen <110621544+DariaLen@users.noreply.github.com>
2023-06-22 23:31:58 +0200

    Update Homework2.java.correct

--- Homework2.java
+++ Homework2.java
@@ -4,7 +4,7 @@ public class Homework2 {
     public static void main(String[] args) {
         int number = 456;
 
-        int num1 = (number % 1000) / 100;
+        int num1 = number / 100;
         int num2 = (number % 100) / 10;    //ili  int num2 = (number / 10) % 10;
         int num3 = number % 10;
 
