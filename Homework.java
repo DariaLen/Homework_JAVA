ad1dd90e5aee4dbf8dc1800c8684dc9608d52e39
DariaLen <110621544+DariaLen@users.noreply.github.com>
2023-06-22 23:05:52 +0200

    Homework_22.06

---
+++ Homework.java
@@ -0,0 +1,32 @@
+package org;
+
+public class Homework {
+
+    public static void main(String[] args) {
+        char myChar = 'G';
+        int myInt = 89;
+        byte myByte = 4;
+        short myShort = 56;
+
+        float myFloat = 4.7333436F;
+        double myDouble = 4.355453532;
+
+        long myLong = 12121;
+
+
+
+
+
+
+        System.out.println("Charter: " + myChar);
+        System.out.println("Integer: " +  myInt);
+        System.out.println("Byte: " + myByte);
+        System.out.println("Short: " + myShort);
+        System.out.println("Float: " + myFloat);
+
+        System.out.println("Double: " + myDouble);
+
+        System.out.println("Long: " +  myLong  );
+
+    }
+}
